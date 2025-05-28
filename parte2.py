ac24e6151b6a4ac38fab6336eef68098af7156ec
belentellechea <141636481+belentellechea@users.noreply.github.com>
2025-05-28 11:36:51 -0300

    parte 2 archivo

---
+++ parte2.py
@@ -0,0 +1,10 @@
+import time
+import math
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#---------------------------------------------
+# PARTE 2: SIMULACIÓN DE VARIABLES ALEATORIAS
+# CONTINUAS
+#---------------------------------------------
+
